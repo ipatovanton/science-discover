e44de873409359c8195a70158b6369f8b9fba1f4
Anton <ipatovanton@gmail.com>
2021-09-20 13:24:22 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,5 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.require_master_key = true
+  config.require_master_key = false
 end
